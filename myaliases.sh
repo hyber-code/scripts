8fba3ecbabf7b3298eefdbece15ef508c4fab221
Xiobe <Xiobe@users.noreply.github.com>
2017-08-21 10:42:04 +0200

    Create myaliases.sh
    Common aliases I use

---
+++ myaliases.sh
@@ -0,0 +1,4 @@
+echo "alias type='file'" >> $HOME/.bash_aliases
+echo "alias fck='sudo \$(history -p \!\!)'" >> $HOME/.bash_aliases
+echo "alias ubuntuversion='lsb_release -a'" >> $HOME/.bash_aliases
+source $HOME/.bash_aliases
