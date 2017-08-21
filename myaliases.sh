59657f4074ed1f91e7b21bd20410db6acff56070
Xiobe <Xiobe@users.noreply.github.com>
2017-08-21 10:51:10 +0200

    Update myaliases.sh with shebang

--- myaliases.sh
+++ myaliases.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "alias type='file'" >> $HOME/.bash_aliases
 echo "alias fck='sudo \$(history -p \!\!)'" >> $HOME/.bash_aliases
 echo "alias ubuntuversion='lsb_release -a'" >> $HOME/.bash_aliases
