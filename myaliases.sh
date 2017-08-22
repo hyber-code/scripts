fd7edad294eaec594ac38e9b9767fd84a589df4a
Xiobe <Xiobe@users.noreply.github.com>
2017-08-22 09:47:21 +0200

    Update myaliases.sh: added ls

--- myaliases.sh
+++ myaliases.sh
@@ -2,4 +2,5 @@
 echo "alias type='file'" >> $HOME/.bash_aliases
 echo "alias fck='sudo \$(history -p \!\!)'" >> $HOME/.bash_aliases
 echo "alias ubuntuversion='lsb_release -a'" >> $HOME/.bash_aliases
+echo "alias ls='ls -alh --full-time --color'" >> $HOME/.bash_aliases
 source $HOME/.bash_aliases
