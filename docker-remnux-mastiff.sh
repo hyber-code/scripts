d88c121a33e188b01d998e5ceec1de71f1573088
Xiobe <Xiobe@users.noreply.github.com>
2017-08-22 14:14:35 +0200

    Update docker-remnux-mastiff.sh: condition workdir

--- docker-remnux-mastiff.sh
+++ docker-remnux-mastiff.sh
@@ -1,6 +1,8 @@
 #/bin/sh
 sudo docker pull remnux/mastiff
 
-cd $HOME
-mkdir mastiff-workdir
-chmod a+xwr mastiff-workdir
+# create the work directory if it doesn't exist
+if [ ! -d $HOME/mastiff-workdir ]; then
+    mkdir mastiff-workdir
+    chmod a+xwr mastiff-workdir
+fi
