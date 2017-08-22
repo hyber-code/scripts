0162420a8822769e1d9e70d1abbdb4d5d33b43dc
Xiobe <Xiobe@users.noreply.github.com>
2017-08-22 16:08:05 +0200

    Update docker-remnux-viper.sh: workdir condition

--- docker-remnux-viper.sh
+++ docker-remnux-viper.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 sudo docker pull remnux/viper
-cd $HOME
-mkdir viper-workdir
-chmod a+xwr viper-workdir
+
+# create the work directory if it doesn't exist
+if [ ! -d $HOME/viper-workdir ]; then
+    mkdir viper-workdir
+    chmod a+xwr viper-workdir
+fi
+
 sudo docker run -d --rm -p 9090:9090 -v ~/viper-workdir:/home/nonroot/workdir remnux/viper
