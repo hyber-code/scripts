71fd4c1598b3ebfe310f03b70167b2fc50786838
Xiobe <Xiobe@users.noreply.github.com>
2017-08-22 09:54:28 +0200

    Update myaliases.sh: added ..

--- myaliases.sh
+++ myaliases.sh
@@ -5,4 +5,5 @@ echo "alias ubuntuversion='lsb_release -a'" >> $HOME/.bash_aliases
 echo "alias ls='ls -alh --full-time --color'" >> $HOME/.bash_aliases
 echo "alias .='ls -alh --full-time --color | more'" >> $HOME/.bash_aliases
 echo "alias cd..='cd ..'" >> $HOME/.bash_aliases
+echo "alias ..='cd ..'" >> $HOME/.bash_aliases
 source $HOME/.bash_aliases
