5f6c1be1f9d70b6147fdcabc30b222960d815ee9
Xiobe <Xiobe@users.noreply.github.com>
2017-08-23 11:26:48 +0200

    Update docker-certsocietegenerale-fir.sh: --name
    "--name fir" added to docker run

--- docker-certsocietegenerale-fir.sh
+++ docker-certsocietegenerale-fir.sh
@@ -4,4 +4,4 @@ mkdir fir
 cd fir
 wget -N "https://raw.githubusercontent.com/certsocietegenerale/FIR/master/docker/Dockerfile"
 sudo docker build -t certsocietegenerale/fir .
-sudo docker run -d --rm -p 8000:8000 certsocietegenerale/fir
+sudo docker run --name fir -d --rm -p 8000:8000 certsocietegenerale/fir
